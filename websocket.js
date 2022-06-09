66b7d0f0620cb0fa05f6e853eab8f9d81446b00b
kangjun55 <Kangjun55@hotmail.com>
2022-06-09 16:11:13 +0900

    Create websocket.js

---
+++ websocket.js
@@ -0,0 +1,35 @@
+class websocket {
+  constructor(_endpoint) {
+    this.ws = new WebSocket(_endpoint);
+  }
+
+  open() {
+    this.ws.onopen = function(e) {
+      console.log("onopen:", e);
+      console.log("onopen: readyState:", this.ws.readyState); // readyState:1
+    };
+
+    this.ws.onmessage = function(e) {
+      console.log("onmessage:", e.data); // data from server
+      console.log("onmessage: readyState:", this.ws.readyState); // readyState:1
+    };
+
+    this.ws.onerror = function(err) {
+      console.log("onerror:", err);
+      console.log("onerror: readyState:", this.ws.readyState);
+      // reconnect:
+      if (this.ws.readyState === 3) // if connection is closed... 
+        this.ws = createWebSocketInstance(MONAJPY_ENDPOINT);
+    };
+
+    this.ws.onclose = function(e) {
+      console.log("onclose:", e);
+      console.log("onclose: readyState:", this.ws.readyState); // readyState:3
+    };
+  }
+
+  close() {
+    console.log("closing...");
+    this.ws.close();
+  }
+}
