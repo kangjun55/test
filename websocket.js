41e202817c96a4507bd3ebfc7ede2ce6a3872471
kangjun55 <Kangjun55@hotmail.com>
2022-06-09 16:15:43 +0900

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -1,9 +1,10 @@
 class websocket {
   constructor(_endpoint) {
+    this._endpoint = _endpoint;
     this.ws = new WebSocket(_endpoint);
   }
 
-  open() {
+  open(onmessage) {
     this.ws.onopen = function(e) {
       console.log("onopen:", e);
       console.log("onopen: readyState:", this.ws.readyState); // readyState:1
@@ -12,14 +13,17 @@ class websocket {
     this.ws.onmessage = function(e) {
       console.log("onmessage:", e.data); // data from server
       console.log("onmessage: readyState:", this.ws.readyState); // readyState:1
+      
+      onmessage();
     };
 
     this.ws.onerror = function(err) {
       console.log("onerror:", err);
       console.log("onerror: readyState:", this.ws.readyState);
+      
       // reconnect:
       if (this.ws.readyState === 3) // if connection is closed... 
-        this.ws = createWebSocketInstance(MONAJPY_ENDPOINT);
+        this.ws = new WebSocket(this._endpoint);
     };
 
     this.ws.onclose = function(e) {
